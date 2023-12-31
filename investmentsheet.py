a8b03bc51b50f35b2dfc697175a0169d4dcc70dd
do0x0ob@protonmail.com <do0x0ob@protonmail.com>
2023-12-31 14:21:51 +0800

    - investment sheet function 'open_file' added an option to adapt api_getter. - add params to call function. 'read_data', 'write_data'

--- investmentsheet.py
+++ investmentsheet.py
@@ -13,22 +13,28 @@ class InvestmentSheet:
         )
         self.client = gspread.authorize(creds)
 
-    def open_file(self):
+    def open_file(self, sheet_type):
         file_name = self.config['spread_sheet']['filename']
-        sheet_name = self.config['spread_sheet']['spreadsheet_name']
+        # sheet_type option: fund, crypto
+        if sheet_type == 'fund':
+            sheet_name = self.config['spread_sheet']['fund_spreadsheet_name']
+        elif sheet_type == 'crypto':
+            sheet_name = self.config['spread_sheet']['crypto_id_spreadsheet_name']
+
         sheet = self.client.open(file_name).worksheet(sheet_name)
         return sheet
         
-    def read_data(self):
-        sheet = self.open_file()
+    def read_data(self, sheet_type):
+        sheet = self.open_file(sheet_type)
         data = sheet.get_all_values()
         df = pd.DataFrame(data[1:], columns=data[0])
         return df
     
-    def write_data(self, data):
-        sheet = self.open_file()
+    def write_data(self, sheet_type, data):
+        sheet = self.open_file(sheet_type)
         j = 2
 
         for i in data:
             sheet.update_acell(f'H{j}', i['Fund Price Latest'])
             j += 1
+    
--- utils.py
+++ utils.py
@@ -9,11 +9,12 @@ def load_config(file_path):
     config.read(file_path)
     return config
 
-async def main(show_or_write=1):
+
+async def update_all_price(show_or_write=1):
     config = load_config('config.ini')
     sheet = InvestmentSheet(config)
     sheet.load_credentials()
-    fund_code_df = sheet.read_data()
+    fund_code_df = sheet.read_data('fund')
     
     crawler = Crawler(config)
     
@@ -23,10 +24,16 @@ async def main(show_or_write=1):
     if show_or_write == 0: # show only
         fund_data_price_df = pd.DataFrame(fund_data_price)
         display(fund_data_price_df)
+
     elif show_or_write == 1: # write only
-        sheet.write_data(fund_data_price)
+        sheet.write_data('fund', fund_data_price)
+        print('Data updated!')
+
     elif show_or_write == 2: # do both
         fund_data_price_df = pd.DataFrame(fund_data_price)
         display(fund_data_price_df)
-        sheet.write_data(fund_data_price)
+        sheet.write_data('fund', fund_data_price)
+        print('Data updated!')
+        
     return None
+
