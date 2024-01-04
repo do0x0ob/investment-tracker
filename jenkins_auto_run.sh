ef1c45065f5bf76ceb906d5ddc80825662dc86d0
YY__Hsieh <do0x0ob@protonmail.com>
2024-01-04 21:14:28 +0800

    Update jenkins_auto_run.sh

--- jenkins_auto_run.sh
+++ jenkins_auto_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source yourpath//investment-tracker/venv/trackerenv/bin/activate
+source yourpath/investment-tracker/venv/trackerenv/bin/activate
 python3 /path/to/your/main.py
 deactivate
